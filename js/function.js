044a899cb026d45b9e0680af5d002003f7459186
Ariel Saravia <diciembre93@gmail.com>
2023-02-28 23:55:20 -0300

    tercer commit

--- js/function.js
+++ js/function.js
@@ -10,5 +10,5 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 5000); // Cambia de imagen cada 2 segundos
+  setTimeout(showSlides, 2000); // Cambia de imagen deacuerdo al tiempo especificado
 }
